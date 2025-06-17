ec0a4453758424352285e5f536e348f5d9d609d2
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2025-06-17 11:56:41 +0530

    First commit

---
+++ ASWebApp/Controllers/IndexController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+        private readonly ILogger<IndexController> _logger;
+
+        public IndexController(ILogger<IndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "/")]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Server is running"});
+        }
+    }
+}
---
+++ ASWebApp/Readme.md
@@ -0,0 +1,23 @@
+﻿# Create variable to be used in workflow file as they are not secret
+
+GitHub Repo → Settings → Secrets and Variables → Actions → Variables
+
+Replace ? with appropriate values
+
+```bash
+AWS_BUCKET=?
+AWS_REGION=?
+AWS_CODE_BUILD_PROJECT_NAME=?
+```
+
+
+# Create variable to be used in workflow file
+
+GitHub Repo → Settings → Secrets and Variables → Actions → Secrets
+
+Replace ? with appropriate values
+
+```bash
+AWS_ACCESS_KEY_ID=?
+AWS_SECRET_ACCESS_KEY=?
+```
